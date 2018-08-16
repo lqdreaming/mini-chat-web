8cee3172752eaad29523bfad6e9ffd204e6224ea
guanda.he <guanda.he@zhenai.com>
2018-08-16 10:11:38 +0800

    fix bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import MatchmakerIndex from '@/components/MatchmakerIndex'
 import UserIndex from '@/components/UserIndex'
 
